c57b480b7b6bf7dbf662a1b036e977afb43b6180
dneovu <113314302+dneovu@users.noreply.github.com>
2023-09-28 00:25:54 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = []
+	let brackets = {}
+
+	bracketsConfig.forEach(e => {
+		brackets[e[1]] = e[0]
+	});
+
+	for (let i = 0; i < str.length; i++) {
+		const current = str[i]
+		if (Object.values(brackets).includes(current)) { // opened
+			
+			if (brackets[current]) { // for equal brackets
+				if (stack[stack.length - 1] === current) {
+					stack.pop() 
+				} else {
+					stack.push(current)
+				}
+			} else {
+				stack.push(current)
+			}
+		}
+
+		else { // closed
+			if (brackets[current] != stack.pop()) return false
+	 	}
+	}
+	
+	return stack.length === 0
 }
+
+console.log(module.exports('|()|(||)||', [['(', ')'], ['|', '|']]))
